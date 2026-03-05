a06fa762079a8ec8a72ad4868c0da1a1299b663b
David-Ammon-Student <daama6e@student.oaisd.org>
2026-03-04 19:26:16 -0500

    added player to move left and right, jump onny when contact to ground. moving obstacle triangle

---
+++ Assets/Block Jumper/Scripts/Obstical.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // TODO: A public float variable to control how fast the obstacle moves across the screen
+    public float zoooom = 5f;
+    
+    // TODO: A public float variable to control how far the object should go before being destroyed offscreen.
+    public float breakBoom = -15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: Move the obstacle to the left at a constant rate. 
+        Vector2 forceDirection = -transform.right;
+        transform.Translate(forceDirection * zoooom * Time.deltaTime);
+        // Make sure this calculation is frame rate independent (hint: use Time.deltaTime) ohh thats right
+        
+
+        // TODO: If the obstalce is off screen to the left, destroy this GameObject (hint: Destroy(gameObject))
+        if (transform.position.x < breakBoom)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
